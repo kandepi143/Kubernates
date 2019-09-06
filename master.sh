245ecb60dcd21c592bf64d4d9848302dc8c550ff
kandepi143 <46445930+kandepi143@users.noreply.github.com>
2019-09-07 01:58:38 +0530

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,43 @@
+# install, start & enable docker service
+yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
+yum install -y -q docker-ce >/dev/null 2>&1
+
+systemctl start docker
+systemctl enable docker
+
+# Disable SELinux
+setenforce 0
+sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+
+#systemctl stop firewalld
+#systemctl disable firewalld
+
+# Disable swap
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+# Update sysctl settings for Kubernates networkin
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+
+# add yum repository
+cat >>/etc/yum.repos.d/kubernetes.repo<<EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+# Install Kubernates & start kubelet
+yum install kubeadm kubelet kubectl -y
+systemctl start kubelet
+systemctl enable kubelet
